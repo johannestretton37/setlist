5c67ac96b432905284891fe4ee582de87d3cdcf1
Johannes Borgström <johannes@highspirits.se>
2018-01-20 23:50:51 +0100

    Correct redirect uris in prod

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,6 +4,6 @@ require('dotenv').config()
 module.exports = {
   NODE_ENV: '"production"',
   SPOTIFY_CLIENT_ID: '"' + process.env.SPOTIFY_CLIENT_ID + '"',
-  REDIRECT_URI_ENC: '"' + process.env.LOCAL_REDIRECT_URI_ENC + '"',
-  REDIRECT_URI: '"' + process.env.LOCAL_REDIRECT_URI + '"'
+  REDIRECT_URI_ENC: '"' + process.env.REDIRECT_URI_ENC + '"',
+  REDIRECT_URI: '"' + process.env.REDIRECT_URI + '"'
 }
