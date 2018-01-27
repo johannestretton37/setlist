2ea35910674ec73332b3e733448c9cca0f3c18ef
Johannes Borgström <johannes.borgstrom@tretton37.com>
2018-01-27 23:56:25 +0100

    Update test

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -12,7 +12,7 @@ module.exports = {
       .url(devServer)
       .waitForElementVisible('#app', 5000)
       // .assert.elementPresent('.new-song-form')
-      .assert.containsText('h1', 'Login')
+      .assert.containsText('h1', 'Log in')
       // .assert.elementCount('img', 1)
       .end()
   }
